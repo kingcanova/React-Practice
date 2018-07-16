8ea25a6f8348e493f1a86b4c27ea77537094eae0
Canova <20795963@Americas.OneACS.com>
2018-07-16 13:05:40 -0400

    again!

--- to-do/client/src/App.js
+++ to-do/client/src/App.js
@@ -21,7 +21,6 @@ class App extends Component {
     
     componentDidMount() 
     {
-        const self = this;
         this.loadListItems();
     }
 
@@ -89,9 +88,7 @@ class App extends Component {
             </form>
             <p></p>
             <div className = "tableDiv">
-            {this.state && this.state.items &&
                 <Table items = {this.state.items} checked = {this.state.checked} delete = {this.delete.bind(this)}/>
-            }
             </div>
           </div>
         );
