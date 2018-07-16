c50de25c12cec5c0780169d0ef251fefd1ea5c70
KingCanova <tcanova1@gmail.com>
2018-07-16 13:52:57 -0400

    work

--- to-do/server/server.js
+++ to-do/server/server.js
@@ -34,12 +34,14 @@ router.get('/', (req, res) => {
 router.get('/items', async (req,res) => 
 {
   //console.log('Got to .get');
+  var info;
   const items = await listItem.find({},(err,items) =>
   {
-    if(err) return res.json({success: false, error:err});
+    if(err) info = res.json({success: false, error:err});
     //console.log('Items in list: ' + items);
-    items = res.json({success: true, data: items});
+    info = res.json({success: true, data: items});
   }); 
+  return info;
 });
 
 router.post('/items',(req,res) =>
